8fb788dabe7a491f44cf69456feab82b2a416359
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2023-12-27 20:49:19 +0530

    Update extensions.py

--- problem_set_1/extensions.py
+++ problem_set_1/extensions.py
@@ -1,33 +1,16 @@
-'''
-file = input("File name: ").strip()
-
-mime_types = ('.gif', '.jpg', '.jpeg', '.png', '.pdf', '.txt', '.zip')
-
-if file.endwith(mime_types) == '.gif':
-    print("image/gif")
-else:
-    print("application/octet-stream")
-'''
-
 file = input("File name: ").strip().lower()
 
-mime_types = ('.gif', '.jpg', '.jpeg', '.png', '.pdf', '.txt', '.zip')
-
-# Check if the file ends with any of the provided mime_types
-for mime_type in mime_types:
-    if file.endswith(mime_type):
-        if mime_type == '.gif':
-            print("image/gif")
-        elif mime_type == '.jpg' or mime_type == '.jpeg':
-            print("image/jpeg")
-        elif mime_type == '.png':
-            print("image/png")
-        elif mime_type == '.pdf':
-            print("application/pdf")
-        elif mime_type == '.txt':
-            print("text/plain")
-        elif mime_type == '.zip':
-            print("application/zip")
-        break
+if file.endswith('.gif'):
+    print("image/gif")
+elif file.endswith('.jpg') or file.endswith('.jpeg'):
+    print("image/jpeg")
+elif file.endswith('.png'):
+    print("image/png")
+elif file.endswith('.pdf'):
+    print("application/pdf")
+elif file.endswith('.txt'):
+    print("text/plain")
+elif file.endswith('.zip'):
+    print("application/zip")
 else:
     print("application/octet-stream")
