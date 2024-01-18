359a25a247091e1758fbb270ca72dcfd6b86f4bb
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-01-18 15:24:18 +0530

    Update test_bank.py

--- problem_set_5/test_bank/test_bank.py
+++ problem_set_5/test_bank/test_bank.py
@@ -1,1 +1,17 @@
 from bank import value
+
+def test_Hello():
+    assert value("Hello") == 0
+    assert value("hello") == 0
+    assert value("hello, world") == 0
+
+def test_Hi():
+    assert value("Hi") == 20
+    assert value("hi") == 20
+    assert value("Hi, World    ") == 20
+
+
+def test_others():
+    assert value("Welcome!") == 100
+    assert value("What's up?") == 100
+    assert value("What's happening?") == 100
